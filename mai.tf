c5ea05a7e83f8b8c9dc0426535672d0d257d117b
manikandan <rajavetrivelmani@gmail.com>
2024-06-26 22:28:33 +0530

    test

--- mai.tf
+++ mai.tf
@@ -2,16 +2,3 @@ provider "aws" {
   region = var.region
 }
 
-module "rds_dev" {
-  source  = "./modules/rds"
-  
-}
-
-module "rds_prod" {
-  source  = "./modules/rds"
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> ab1fa2a8fe73022622dd75ae6f2ec5ff27e31e7e
-}
